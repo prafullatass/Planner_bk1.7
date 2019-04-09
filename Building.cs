be9b76dc25a1d5467a8128b86ed84fb4279ff123
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-09 12:08:56 -0500

    first commit

---
+++ Building.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Planner {
+    class Building {
+        private string _designer = "Prafullata Sonawane";
+        private DateTime _dateConstructed  {get;set; }
+        private string _address {get;set; }
+        private string _owner {get;set; }
+        public int Stories {get;set; }
+        public double Width  {get;set; }
+        public double Depth {get;set; }
+        public double Volume  {
+            get {
+            return Width * Depth * 3 * Stories;
+            }
+        }
+
+        //methods
+        public Building (string address ){
+            _address = address;
+            _dateConstructed = DateTime.Now;
+        }
+        public void Purchase (string newOwner) {
+            _owner = newOwner;
+        }
+
+        public string getAddress () {
+            return _address;
+        }
+
+        public string getDesigner () {
+            return _designer;
+        }
+
+        public string getOwner () {
+            return _owner;
+        }
+        public DateTime getDateConstructed () {
+            return _dateConstructed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            FiveOneTwoEigth.Depth = 3000.90;
+            FiveOneTwoEigth.Width = 100.90;
+            FiveOneTwoEigth.Stories =10;
+            FiveOneTwoEigth.Purchase("Mira Sutar");
+
+            List<Building> VirtualCity = new List<Building>();
+            VirtualCity.Add(FiveOneTwoEigth);
+
+            foreach (Building building in VirtualCity)
+            {
+                Console.WriteLine($@"
+                    {building.getAddress()}
+                    ----------------------------------------------
+                    Designed By : {building.getDesigner()}
+                    Constructed on {building.getDateConstructed()}
+                    Owned by {building.getOwner()}
+                    {building.Volume} cubic meters of space
+                 ");
+            }
+        }
+    }
+}
